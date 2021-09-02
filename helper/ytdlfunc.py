1d72751eafd2697beccac9365facdd1d882aaabb
mirror-ravindu-1 <89637605+mirror-ravindu-1@users.noreply.github.com>
2021-09-02 11:26:04 +0530

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -8,10 +8,10 @@ import asyncio
 def buttonmap(item):
     quality = item['format']
     if "audio" in quality:
-        return [InlineKeyboardButton(f"{quality} Music 🔥 {humanbytes(item['filesize'])}",
+        return [InlineKeyboardButton({quality} Music 🔥 {humanbytes(item['filesize'])}",
                                      callback_data=f"ytdata||audio||{item['format_id']}||{item['yturl']}")]
     else:
-        return [InlineKeyboardButton(f"{quality} Video 📽 {humanbytes(item['filesize'])}",
+        return [InlineKeyboardButton({quality} Video 📽 {humanbytes(item['filesize'])}",
                                      callback_data=f"ytdata||video||{item['format_id']}||{item['yturl']}")]
 
 # Return a array of Buttons
