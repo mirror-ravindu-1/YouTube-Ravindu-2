865824da143ff14ff87baf3069a011f3fc403c67
mirror-ravindu-1 <89637605+mirror-ravindu-1@users.noreply.github.com>
2021-10-26 22:32:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
 BOT_TOKEN = '1965442627:AAGdG_3vPtmzP_LwzYHb82Qmqj5yS90ugDA'
-APP_ID = 7286290'
-API_HASH = 729dc1710bbb7501c999673522801d44'
+APP_ID = '7286290'
+API_HASH = '729dc1710bbb7501c999673522801d44'
 
 youtube_next_fetch = 0  # time in minute
 
